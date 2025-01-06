33f43d433fbb1493dc04abba64d60a0ae3bd356a
Majdi77 <Majdi.melliti@esprit.tn>
2025-01-06 21:43:02 +0100

    06/01

--- components/host-special/AccessoryItem.vue
+++ components/host-special/AccessoryItem.vue
@@ -1,39 +1,32 @@
 <template>
-  <div
-    :class="[
-      'flex gap-6 justify-between p-8 pb-16 w-full rounded-3xl border-2 border-solid min-h-[106px] max-md:px-5 max-md:max-w-full',
-      selected ? 'bg-neutral-100 border-black' : 'bg-white border-black border-opacity-10',
-      'max-md:flex-col max-md:gap-6 max-md:items-start'
-    ]"
-  >
+  <div :class="[
+    'flex gap-6 justify-between p-8 pb-16 w-full rounded-3xl border-2 border-solid min-h-[106px] max-md:px-5 max-md:max-w-full',
+    selected ? 'bg-neutral-100 border-black' : 'bg-white border-black border-opacity-10',
+    'max-md:flex-col max-md:gap-6 max-md:items-start'
+  ]">
     <!-- Image and Title Section -->
     <div class="flex items-center gap-6 w-full md:w-auto lg:w-[633px]">
-      <img
-        :src="icon"
-        :alt="title"
-        class="object-contain shrink-0 self-stretch my-auto aspect-square w-[30px]"
-      />
+      <img :src="icon" :alt="title" class="object-contain shrink-0 self-stretch my-auto aspect-square w-[30px]" />
       <span class="my-auto">{{ title }}</span>
     </div>
 
     <!-- Checkbox Section -->
-    <label class="checkbox-label flex items-center gap-4">
-      <input
-        type="checkbox"
-        class="checkbox-input"
-        :checked="selected"
-        @change="handleChange"
-      />
-      <span class="checkbox-custom flex items-center justify-center">
-        <img
-          loading="lazy"
+
+
+
+    <label class="checkbox-label">
+      <input type="checkbox" class="checkbox-input" :checked="selected" @change="handleChange" />
+      <span class="checkbox-custom">
+        <img loading="lazy"
           src="https://cdn.builder.io/api/v1/image/assets/TEMP/466c3b17040ec4fe2c82fa66cd685d4ef01d53b257e7e62f263d2503632fe529?placeholderIfAbsent=true&apiKey=cefca70c5e3e4c30aa4a14ad34b27ffa"
-          alt="Checkbox Icon"
-          class="object-contain aspect-square w-[30px]"
-        />
-        <span v-if="selected" class="checkmark">&#10003;</span>
+          alt="" class="object-contain shrink-0 self-stretch my-auto aspect-square w-[30px] mt-2 md:mt-0">
+        <span class="checkmark">&#10003;</span>
       </span>
     </label>
+
+   
+
+
   </div>
 </template>
 
@@ -74,7 +67,8 @@ export default defineComponent({
   display: flex;
   align-items: center;
   cursor: pointer;
-  gap: 10px; /* Increased gap between checkbox and label */
+  gap: 10px;
+  /* Increased gap between checkbox and label */
 }
 
 .checkbox-input {
@@ -106,4 +100,3 @@ export default defineComponent({
   color: black;
 }
 </style>
-
