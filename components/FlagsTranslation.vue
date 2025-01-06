46b1f1f02857960a6da90e74e9ae7c05c0455716
Majdi77 <Majdi.melliti@esprit.tn>
2025-01-06 21:01:14 +0100

    06/01

--- components/FlagsTranslation.vue
+++ components/FlagsTranslation.vue
@@ -11,7 +11,7 @@
       enter-to-class="transform opacity-100 scale-100" leave-active-class="transition ease-in duration-75"
       leave-from-class="transform opacity-100 scale-100" leave-to-class="transform opacity-0 scale-95">
       <MenuItems
-        class="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+        class="absolute right-0  origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
         <div class="py-1">
           <MenuItem v-slot="{ active }">
             <a @click="changeLanguage(englishFlag)" :class="[active ? 'bg-gray-100 text-gray-900' : 'text-gray-700', 'block px-4 py-2 text-sm']">
